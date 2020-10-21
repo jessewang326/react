d0b2a1937cbbf1d14f9ce4d55d412e4d61563882
Jiaxi Wang <jwang724@uwo.ca>
2020-10-21 09:57:46 -0400

    test

---
+++ build/precache-manifest.5c84d0b14c8debcbfaf9ab933999e62f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ffe9b76cb87f277f4d2bf9eb8fbb90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1088adbd3a35303a69c8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a46617966b536919ab2",
+    "url": "/static/js/2.68f56c50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.68f56c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1088adbd3a35303a69c8",
+    "url": "/static/js/main.2819a602.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
